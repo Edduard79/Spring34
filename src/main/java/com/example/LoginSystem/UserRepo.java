6467ce87cdda356024f4304a460f0794fa876f8f
Edduard79 <eduard.22@gmail.com>
2023-06-13 13:58:38 +0200

    Hope it works

---
+++ src/main/java/com/example/LoginSystem/UserRepo.java
@@ -0,0 +1,9 @@
+package com.example.LoginSystem;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
